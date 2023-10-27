f6031628c306580337b1dc949f6ededdf9b42076
absakran01 <146729719+absakran01@users.noreply.github.com>
2023-10-27 21:04:01 +0300

    Security00

---
+++ src/main/java/com/example/CMS_01/security/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.CMS_01.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+}
